61785d3e7985aeebee5afa811556cbe2fd6f4915
Rien Ilagan <rienman88@gmail.com>
2026-06-26 21:37:39 +0800

    Refactor Container component for simplified structure

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -2,25 +2,9 @@ type Props = {
   children: React.ReactNode;
 };
 
-export default function Container({
-  children,
-}: Props) {
+export default function Container({ children }: Props) {
   return (
-    <div
-      className="
-        mx-auto
-
-        w-full
-
-        max-w-screen-2xl
-
-        px-6
-
-        lg:px-10
-
-        xl:px-14
-      "
-    >
+    <div className="mx-auto max-w-7xl px-6">
       {children}
     </div>
   );
