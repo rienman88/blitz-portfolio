cfff17a083adad0dbb3d136d595abe2b57f1d25c
Rien Ilagan <rienman88@gmail.com>
2026-06-26 20:53:54 +0800

    Update Footer component for responsive design

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="border-t border-slate-800 py-8">
 
-      <div className="flex justify-between text-center text-gray-500">
+      <div className="hidden md:flex justify-between text-center text-gray-500">
 
         © Blitz 2026 Rienart Ryan Ilagan
 
