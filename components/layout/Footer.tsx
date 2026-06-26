74a93a364c7996670150f9bc5c5287cc673ec150
Rien Ilagan <rienman88@gmail.com>
2026-06-26 21:12:46 +0800

    Update Footer component layout structure

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="border-t border-slate-800 py-8">
 
-      <div className="hidden md:flex justify-between text-center text-gray-500">
+      <div className="flex flex-col min-h-screen text-center text-gray-500">
 
         © Blitz 2026 Rienart Ryan Ilagan
 
