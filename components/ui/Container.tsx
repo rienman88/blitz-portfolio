71cc8884ca234bda1c1e5da1abb4e9af1ce99c66
Rien Ilagan <rienman88@gmail.com>
2026-06-26 19:31:27 +0800

    Refactor Container component for better readability

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -2,9 +2,25 @@ type Props = {
   children: React.ReactNode;
 };
 
-export default function Container({ children }: Props) {
+export default function Container({
+  children,
+}: Props) {
   return (
-    <div className="mx-auto max-w-7xl px-6">
+    <div
+      className="
+        mx-auto
+
+        w-full
+
+        max-w-screen-2xl
+
+        px-6
+
+        lg:px-10
+
+        xl:px-14
+      "
+    >
       {children}
     </div>
   );
