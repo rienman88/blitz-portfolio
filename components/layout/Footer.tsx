866514289a8659607b244530b0ae9fa7603b36cd
Rien Ilagan <rienman88@gmail.com>
2026-06-26 20:01:22 +0800

    Update Footer layout for better alignment

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="border-t border-slate-800 py-8">
 
-      <div className="mx-auto max-w-7xl px-6 text-center text-gray-500">
+      <div className="flex justify-between text-center text-gray-500">
 
         © Blitz 2026 Rienart Ryan Ilagan
 
